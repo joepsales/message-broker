7db4568f8d302016afcd29ffd4295b7a7158a7dd
Joep Sales <info@incomtechdesign.com>
2022-05-06 20:34:07 +0200

    Added log topic

---
+++ receive.js
@@ -0,0 +1,26 @@
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', (error0, connection) => {
+    if (error0) {
+        throw error0;
+    }
+
+    connection.createChannel((error1, channel) => {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'hello';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        console.log(' [x] Waiting for messages in %s. To exit press control+C', queue);
+
+        channel.consume(queue, (msg) => {
+            console.log(' [x] Received %s', msg.content.toString());
+        }, {
+            noAck: true
+        });
+    });
+});
